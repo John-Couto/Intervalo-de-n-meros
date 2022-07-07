04e135b338268ffc32dc44ea99a79671415a5c57
John Couto Silva <johndevprof@gmail.com>
2022-07-07 19:40:25 -0300

    Arrumado Problema do looping

--- Program.cs
+++ Program.cs
@@ -2,30 +2,39 @@
 Console.WriteLine("Intervalos");
 var lista = new int[] { 100, 101, 102, 103, 104, 105, 110, 111, 113, 114, 115, 150 };
 
+Console.WriteLine("Lista 100 - 105");
 for (int i = 0; i < lista.Length; i++)
-{
-    if (lista[i] >= 100  && lista[i] <= 105)
+{   
+    if (lista[i] >= 100 && lista[i] <=105)
     {
-        Console.WriteLine("Lista 100-105");
         var zeroACentoECinco = lista[i];
         Console.WriteLine(zeroACentoECinco);
     }
-    else if (lista[i] >= 110 && lista[i] <= 111)
+}
+Console.WriteLine("Lista 110 - 111");
+for (int i = 0; i < lista.Length; i++)
+{ 
+    if (lista[i] >= 110 && lista[i] <= 111)
     {
         var centoEDezACentoEQuinze = lista[i];
-        Console.WriteLine("Lista 110-111");
         Console.WriteLine(lista[i]);        
     }
-    else if (lista[i] >= 113 && lista[i] <= 115)
+}
+    Console.WriteLine("Lista 113 - 115");
+for (int i = 0; i < lista.Length; i++) 
+{ 
+    if (lista[i] >= 113 && lista[i] <= 115)
     {
         var centoEDezAcentoEQuinze = lista[i];
-        Console.WriteLine("Lista 113-115");
         Console.WriteLine(lista[i]);        
     } 
-    else 
+}
+Console.WriteLine("lista 150");
+for (int i = 0; i < lista.Length; i++)
+{ 
+    if (lista[i] >= 150)
     { 
-        var centoECinquenta = lista[i];
-        Console.WriteLine("Lista 150");
+        var centoECinquenta = lista[i];;
         Console.WriteLine(lista[i]);        
     }
 }
